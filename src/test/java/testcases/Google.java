35eb7c6545be1fd17dd6819716413dbd29c01dd5
Raju <ragiraju78@gmail.com>
2021-02-14 22:42:18 +0200

    my second commit

---
+++ src/test/java/testcases/Google.java
@@ -0,0 +1,35 @@
+package testcases;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+@Test (enabled=true)
+public class Google {
+	public void googleSearch() throws InterruptedException {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); //waits for the presence of elements for 10 seconds
+		
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("q"))).sendKeys("Way 2 Automation");
+		WebElement elm = driver.findElement(By.name("q"));
+		elm.submit();
+		Thread.sleep(1000);
+		//wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("/html/body/ntp-app//div[1]/ntp-realbox//div/input"))).sendKeys("Way 2 Automation");
+				
+		driver.quit();
+		
+	}
+
+}
