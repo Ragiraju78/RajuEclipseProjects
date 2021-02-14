e2b2faec6c2338fab00494c307abd50edf1a1855
Raju <ragiraju78@gmail.com>
2021-02-15 01:33:29 +0200

    my fifth commit

--- src/test/java/testcases/GoogleMain.java
+++ src/test/java/testcases/GoogleMain.java
@@ -8,28 +8,27 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.Test;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-@Test (enabled=true)
-public class Google {
-	public void googleSearch() throws InterruptedException {
+public class GoogleMain {
+	public static void main(String[] args) throws InterruptedException {
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.google.com");
 		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); //waits for the presence of elements for 10 seconds
-		
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS); // waits for the presence of elements for 10
+																			// seconds
+
 		WebDriverWait wait = new WebDriverWait(driver, 10);
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("q"))).sendKeys("Way 2 Automation");
 		WebElement elm = driver.findElement(By.name("q"));
 		elm.submit();
 		Thread.sleep(1000);
-		//wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("/html/body/ntp-app//div[1]/ntp-realbox//div/input"))).sendKeys("Way 2 Automation");
-				
+		// wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("/html/body/ntp-app//div[1]/ntp-realbox//div/input"))).sendKeys("Way
+		// 2 Automation");
+
 		driver.quit();
-		
+
 	}
 
 }
